df4af93aaee1ed5a09c69dcf1a67c9977dcd9690
jackiejohnston <jackiejohnston5@gmail.com>
2015-09-05 23:48:15 -0700

    Set up Devise/OmniAuth

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,19 @@
 class User < ActiveRecord::Base
+  def self.create_from_omniauth(params)
+    attributes = {
+      email: params['info']['email'],
+      name: params['info']['name'],
+      password: Devise.friendly_token,
+      confirmation_token: nil,
+      confirmed_at: Time.now.utc
+    }
+
+    create(attributes)
+  end
+
+  has_many :authentications, class_name: 'UserAuthentication', dependent: :destroy
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  devise :omniauthable, :confirmable, :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 end
